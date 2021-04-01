0ab0d6e2ed396bb94815625a543a88d753011bbe
ducktrshessami <jwee761@gmail.com>
2021-03-31 19:22:32 -0700

    open express server on test script

---
+++ index.test.js
@@ -0,0 +1,10 @@
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, function () {
+    describe(`Testing application on PORT ${PORT}`, function () {
+
+    });
+});
